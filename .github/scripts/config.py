dc2312f1e8454214e2c7100d6e5e56ad82f19c86
Alexander Sabanov <alexsab@mail.ru>
2024-03-11 17:02:42 +0400

    Update config.py

--- .github/scripts/config.py
+++ .github/scripts/config.py
@@ -4,5 +4,91 @@ dealer = {
 }
 # Словарь соответствия цветов
 model_mapping = {
+    "X90 PLUS": {
+        "folder": "X90 PLUS",
+        "color": {
+            "White": "c1b.png",
+            "Black": "c2b.png",
+            "Blue": "c3b.png",
+            "Violet": "c4b.png",
+            "Grey": "c5b.png",
+            "Белый": "c1b.png",
+            "Черный": "c2b.png",
+            "Синий": "c3b.png",
+            "Фиолетовый": "c4b.png",
+            "Серый": "c5b.png",
+        }
+    },
+    "Dashing": {
+        "folder": "Dashing",
+        "color": {
+            "White": "c1b.png",
+            "Black": "c2b.png",
+            "Red": "c3b.png",
+            "Azure": "c4b.png",
+            "Grey": "c5b.png",
+            "Green": "c6b.png",
+            "Lightgray": "c7b.png",
+            "Белый": "c1b.png",
+            "Черный": "c2b.png",
+            "Красный": "c3b.png",
+            "Лазурный": "c4b.png",
+            "Серый": "c5b.png",
+            "Зеленый": "c6b.png",
+            "Светло-серый": "c7b.png",
+        }
+    },
+    "X70 PLUS": {
+        "folder": "X70 PLUS",
+        "color": {
+            "White": "c1b.png",
+            "Grey": "c2b.png",
+            "Red": "c3b.png",
+            "Gray": "c4b.png",
+            "Blue": "c5b.png",
+            "Darkblue": "c6b.png",
+            "Black": "c7b.png",
+            "Белый": "c1b.png",
+            "Серый": "c2b.png",
+            "Красный": "c3b.png",
+            "Серый": "c4b.png",
+            "Синий": "c5b.png",
+            "Темно-синий": "c6b.png",
+            "Черный": "c7b.png",
+        }
+    },
+    "X70": {
+        "folder": "X70",
+        "color": {
+            "White": "c1b.png",
+            "Grey": "c2b.png",
+            "Red": "c3b.png",
+            "Gray": "c4b.png",
+            "Blue": "c5b.png",
+            "Darkblue": "c6b.png",
+            "Black": "c7b.png",
+            "Белый": "c1b.png",
+            "Серый": "c2b.png",
+            "Красный": "c3b.png",
+            "Серый": "c4b.png",
+            "Синий": "c5b.png",
+            "Темно-синий": "c6b.png",
+            "Черный": "c7b.png",
+        }
+    },
+    "T2": {
+        "folder": "X70",
+        "color": {
+            "Black": "c1b.png",
+            "Lightgray": "c2b.png",
+            "Gold": "c3b.png",
+            "Gray": "c4b.png",
+            "Черный": "c1b.png",
+            "Светло-серый": "c2b.png",
+            "Золотой": "c3b.png",
+            "Бежевый": "c3b.png",
+            "Серый": "c4b.png",
+        }
+    }
     # ... добавьте другие модели по мере необходимости
 }
